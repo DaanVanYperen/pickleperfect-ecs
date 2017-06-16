d3524503365b22540a99ff180cefabc7c01e095d
DaanVanYperen <daan@mostlyoriginal.net>
2017-06-17 00:42:32 +0200

    Proof of Concept PIXI. :D

--- pickleperfect-jam/pickleperfect-jam-backend-pixi-js/src/main/kotlin/net/mostlyoriginal/pickleperfect/jam/api/PixiApplication.kt
+++ pickleperfect-jam/pickleperfect-jam-backend-pixi-js/src/main/kotlin/net/mostlyoriginal/pickleperfect/jam/api/PixiApplication.kt
@@ -1,18 +1,27 @@
 package net.mostlyoriginal.pickleperfect.jam.api
 
+import kotlin.browser.document
+
 /**
  * @author Daan van Yperen
  */
 class PixiApplication(game: Game) : Application(game) {
     override fun initialize() {
-        game.initialize()
-    }
 
-    override fun process(): Boolean {
-        return game.process(delta)
-    }
+        val options = Options()
 
-    override fun release() {
-        game.dispose()
+        options.backgroundColor = "0x00FF00"
+        options.width = 800
+        options.height = 600
+
+        val application = PIXI.Application(
+                options = options
+        )
+
+        document.body!!.appendChild(application.view)
+
+//        game.initialize()
+//        game.process(0F)
+//        game.dispose()
     }
 }
--- pickleperfect-jam/pickleperfect-jam-backend-pixi-js/src/main/kotlin/net/mostlyoriginal/pickleperfect/jam/api/PixiWrapper.kt
+++ pickleperfect-jam/pickleperfect-jam-backend-pixi-js/src/main/kotlin/net/mostlyoriginal/pickleperfect/jam/api/PixiWrapper.kt
@@ -1,6 +1,27 @@
 package net.mostlyoriginal.pickleperfect.jam.api
 
-//native class PIXI {
-//    public val WEBGL_RENDERER: Int = 0
-//    public val CANVAS_RENDERER: Int = 1
-//}
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.Node
+
+external class Object
+
+
+@JsName("Object")
+external class Options {
+    var width: Int = definedExternally
+    var height: Int = definedExternally
+    var backgroundColor: String = definedExternally
+}
+
+@JsName("PIXI")
+external object PIXI {
+    @JsName("Application")
+    class Application(
+            options: Any? = definedExternally,
+            arg2: Any? = definedExternally,
+            arg3: Any? = definedExternally,
+            arg4: Boolean? = definedExternally,
+            arg5: Boolean? = definedExternally) {
+        var view: Node = definedExternally
+    }
+}
